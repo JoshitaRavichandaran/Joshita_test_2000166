f89a6aed936a64ca32d1bca550460c413e95ea9a
JoshitaRavichandaran <122767496+JoshitaRavichandaran@users.noreply.github.com>
2023-02-02 11:28:29 +0530

    Add files via upload

---
+++ testdata.sql
@@ -0,0 +1,13 @@
+create table employee
+(
+	emp_id integer,
+	name varchar(25),
+	designation varchar(25),
+	email varchar(25),
+	location varchar(25)
+);
+
+insert into employee values 
+(1,'Joshi','HR','jo@gmail.com','Pune'),
+(2,'Sanju','Manager','sa@gmail.com','Banglore'),
+(3,'Tanu','Intern','ta@gmail.com','Chennai');
